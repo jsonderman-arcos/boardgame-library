b4549a72a315c40669961d7c6cdd71a707b46e5d
Jason Sonderman <jsonderman@arcos-inc.com>
2026-01-11 11:58:43 -0600

    Updated Library.tsx

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -629,7 +629,7 @@ export default function Library() {
             ))}
           </div>
         ) : (
-          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-3">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
             {filteredLibrary.map((entry) => (
               <GameCard
                 key={entry.id}
